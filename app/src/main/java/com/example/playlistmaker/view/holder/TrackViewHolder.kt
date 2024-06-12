9beeeab9873f5393320dc755a80682c23309dc47
Anastasiya <personalnastya8@gmail.com>
2024-06-12 23:58:41 +0300

    TASK-10-1 Updates

--- app/src/main/java/com/example/playlistmaker/view/holder/TrackViewHolder.kt
+++ app/src/main/java/com/example/playlistmaker/view/holder/TrackViewHolder.kt
@@ -1,30 +1,34 @@
 package com.example.playlistmaker.view.holder
 
+import android.util.TypedValue
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.example.playlistmaker.R
 import com.example.playlistmaker.entities.Track
 
 class TrackViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    private val trackNameTextView: TextView
-    private val artistNameTextView: TextView
-    private val trackTimeTextView: TextView
-    private val imageView: ImageView
-
-    init {
-        trackNameTextView = itemView.findViewById(R.id.trackName)
-        artistNameTextView = itemView.findViewById(R.id.artistName)
-        trackTimeTextView = itemView.findViewById(R.id.trackTime)
-        imageView = itemView.findViewById(R.id.image)
-    }
+    private val trackNameTextView: TextView = itemView.findViewById(R.id.trackName)
+    private val artistNameTextView: TextView = itemView.findViewById(R.id.artistName)
+    private val trackTimeTextView: TextView = itemView.findViewById(R.id.trackTime)
+    private val imageView: ImageView = itemView.findViewById(R.id.image)
 
     fun bind(model: Track) {
         trackNameTextView.text = model.trackName
         artistNameTextView.text = model.artistName
         trackTimeTextView.text = model.trackTime
-        Glide.with(itemView).load(model.artworkUrl100).placeholder(R.drawable.placeholder).into(imageView)
+        Glide.with(itemView)
+            .load(model.artworkUrl100)
+            .placeholder(R.drawable.placeholder)
+            .transform(RoundedCorners(
+                TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP,
+                    4F,
+                    itemView.resources.displayMetrics).toInt()
+            ))
+            .into(imageView)
     }
 }
