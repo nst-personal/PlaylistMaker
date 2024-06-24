6b800a7602fe8897299d10ac7daaff1d21c6c688
Anastasiya <personalnastya8@gmail.com>
2024-06-24 20:36:54 +0300

    TASK-11-1 Updates

---
+++ app/src/main/java/com/example/playlistmaker/models/TrackResponse.kt
@@ -0,0 +1,6 @@
+package com.example.playlistmaker.models
+
+import com.example.playlistmaker.entities.Track
+
+class TrackResponse (val resultCount: Number,
+                     val results: List<Track>)
