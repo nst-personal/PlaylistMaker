c8e2f222565cb2ef758c7f4bf2d3749b4657ff9e
Anastasiya <personalnastya8@gmail.com>
2024-09-21 20:19:28 +0300

    TASK-16-1 Updates

--- app/src/main/java/com/example/playlistmaker/presentation/ui/settings/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/presentation/ui/settings/SettingsActivity.kt
@@ -19,7 +19,8 @@ class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_settings)
+        binding = ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
