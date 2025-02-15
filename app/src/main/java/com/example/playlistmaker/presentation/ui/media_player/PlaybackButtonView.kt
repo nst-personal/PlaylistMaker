21d0fd09c11bd70eadbffd87eae3ce8b611c6587
Anastasiya <personalnastya8@gmail.com>
2025-02-16 00:33:58 +0300

    TASK-24

---
+++ app/src/main/java/com/example/playlistmaker/presentation/ui/media_player/PlaybackButtonView.kt
@@ -0,0 +1,106 @@
+package com.example.playlistmaker.presentation.ui.media_player
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.graphics.drawable.Drawable
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import androidx.annotation.AttrRes
+import androidx.annotation.StyleRes
+import androidx.core.graphics.drawable.toBitmap
+import com.example.playlistmaker.R
+
+interface OnPlaybackButtonViewClickListener {
+    fun onTouch(isPlaying: Boolean)
+}
+
+class PlaybackButtonView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    @AttrRes defStyleAttr: Int = 0,
+    @StyleRes defStyleRes: Int = 0,
+) : View(context, attrs, defStyleAttr, defStyleRes) {
+
+    private var isPlaying = false
+
+    private val imagePlayBitmap: Bitmap?
+    private val imagePauseBitmap: Bitmap?
+    private val playDrawable: Drawable?
+    private val pauseDrawable: Drawable?
+    private var imageRect = RectF(0f, 0f, 0f, 0f)
+    private var listener: OnPlaybackButtonViewClickListener? = null
+
+    init {
+        context.theme.obtainStyledAttributes(
+            attrs,
+            R.styleable.PlaybackButtonView,
+            defStyleAttr,
+            defStyleRes
+        ).apply {
+            try {
+                playDrawable = getDrawable(R.styleable.PlaybackButtonView_playDrawable)
+                pauseDrawable = getDrawable(R.styleable.PlaybackButtonView_pauseDrawable)
+                playDrawable?.setBounds(
+                    0, 0,
+                    playDrawable.intrinsicWidth, playDrawable.intrinsicHeight
+                )
+                pauseDrawable?.setBounds(
+                    0, 0,
+                    pauseDrawable.intrinsicWidth, pauseDrawable.intrinsicHeight
+                )
+                imagePlayBitmap = playDrawable?.toBitmap()
+                imagePauseBitmap = pauseDrawable?.toBitmap()
+            } finally {
+                recycle()
+            }
+        }
+    }
+
+    fun setOnPlayPauseButtonClickListener(listener: OnPlaybackButtonViewClickListener) {
+        this.listener = listener
+    }
+
+    fun updateState() {
+        isPlaying = !isPlaying
+        invalidate()
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                return true
+            }
+
+            MotionEvent.ACTION_UP -> {
+                listener?.onTouch(isPlaying)
+                return true
+            }
+        }
+        return super.onTouchEvent(event)
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        imageRect = RectF(0f, 0f, measuredWidth.toFloat(), measuredHeight.toFloat())
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        handleImage(canvas)
+    }
+
+    private fun handleImage(canvas: Canvas) {
+        if (!isPlaying) {
+            imagePlayBitmap?.let {
+                canvas.drawBitmap(imagePlayBitmap, null, imageRect, null)
+            }
+        } else {
+            imagePauseBitmap?.let {
+                canvas.drawBitmap(imagePauseBitmap, null, imageRect, null)
+            }
+        }
+    }
+
+}
